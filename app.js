9ec9845d2dd97f16eb44476012c0f9bec39596a5
Ambrish Chaurasia <69149051+ambrish14@users.noreply.github.com>
2026-06-18 11:49:29 +0530

    store the prvate and public key and use the method to encrypt palin text using crypto.publicEncrypt() and later use privateDecrypt to decrypt the data

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const crypto = require("node:crypto");
 const fs = require("node:fs");
-const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
-  modulusLength: 2054, // jey size in bits
-  publicKeyEncoding: {
-    type: "spki", //other option is pkcs1
-    format: "pem",
-  },
-  privateKeyEncoding: {
-    type: "pkcs8", // other option pkcs1
-    format: "pem",
-  },
+// const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+//   modulusLength: 2054, // jey size in bits
+//   publicKeyEncoding: {
+//     type: "spki", //other option is pkcs1
+//     format: "pem",
+//   },
+//   privateKeyEncoding: {
+//     type: "pkcs8", // other option pkcs1
+//     format: "pem",
+//   },
+// });
+
+// console.log(publicKey);
+// console.log(privateKey);
+
+// fs.writeFileSync("private.pem", privateKey);
+// fs.writeFileSync("public.pem", publicKey);
+
+const privateKeyPem = fs.readFileSync("private.pem");
+
+const publicKeyPem = fs.readFileSync("public.pem");
+
+const privateKey = crypto.createPrivateKey({
+  key: privateKeyPem,
+  format: "pem",
 });
 
-console.log(publicKey);
-console.log(privateKey);
+const publicKey = crypto.createPublicKey({
+  key: publicKeyPem,
+  format: "pem",
+});
 
-fs.writeFileSync("private.pem", privateKey);
-fs.writeFileSync("public.pem", publicKey);
+const plainText = Buffer.from("This is my Password $#*U(r&F", "utf-8");
 
-// const privateKeyPem = fs.readFileSync("private.pem");
+const cipherText = crypto.publicEncrypt({ key: publicKey }, plainText);
+console.log(cipherText);
 
-// const publicKeyPem = fs.readFileSync("public.pem");
+crypto.privateEncrypt;
+
+crypto.publicDecrypt;
+crypto.privateDecrypt;
